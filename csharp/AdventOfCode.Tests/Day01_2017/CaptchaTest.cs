ad4279ae5c7737f8c914b64858642eb5018dcddc
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2018-12-01 07:39:09 -0600

    2017-01: Refactor C# unit tests to properly use theories and InlineData

--- csharp/AdventOfCode.Tests/Day01_2017/CaptchaTest.cs
+++ csharp/AdventOfCode.Tests/Day01_2017/CaptchaTest.cs
@@ -7,68 +7,26 @@ namespace AdventOfCode.Tests
     public class CaptchaTest
     {
         [Theory]
-        [InlineData("1122")]
-        public void SumOf3WithStep1(string value)
+        [InlineData("1122", 3)]
+        [InlineData("1111", 4)]
+        [InlineData("1234", 0)]
+        [InlineData("91212129", 9)]
+        public void CorrectlyCalculatesCaptchaWithStep1(string value, int expected)
         {
             int result = Captcha.GetCaptcha(value);
-            Assert.Equal(3, result);
+            Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData("1111")]
-        public void SumOf4WithStep1(string value)
-        {
-            int result = Captcha.GetCaptcha(value);
-            Assert.Equal(4, result);
-        }
-
-        [Theory]
-        [InlineData("1234")]
-        public void SumOf0WithStep1(string value)
-        {
-            int result = Captcha.GetCaptcha(value);
-            Assert.Equal(0, result);
-        }
-
-        [Theory]
-        [InlineData("91212129")]
-        public void SumOf9WithStep1(string value)
-        {
-            int result = Captcha.GetCaptcha(value);
-            Assert.Equal(9, result);
-        }
-
-        [Theory]
-        [InlineData("1212")]
-        public void SumOf6WithStepNdiv2(string value)
+        [InlineData("1212", 6)]
+        [InlineData("1221", 0)]
+        [InlineData("123425", 4)]
+        [InlineData("12131415", 4)]
+        [InlineData("123123", 12)]
+        public void SumOf6WithStepNdiv2(string value, int expected)
         {
             int result = Captcha.GetCaptcha(value, value.Length / 2);
             Assert.Equal(6, result);
-        }
-
-        [Theory]
-        [InlineData("1221")]
-        public void SumOf0WithStepNdiv2(string value)
-        {
-            int result = Captcha.GetCaptcha(value, value.Length / 2);
-            Assert.Equal(0, result);
-        }
-
-        [Theory]
-        [InlineData("123425")]
-        [InlineData("12131415")]
-        public void SumOf4WithStepNdiv2(string value)
-        {
-            int result = Captcha.GetCaptcha(value, value.Length / 2);
-            Assert.Equal(4, result);
-        }
-
-        [Theory]
-        [InlineData("123123")]
-        public void SumOf12WithStepNdiv2(string value)
-        {
-            int result = Captcha.GetCaptcha(value, value.Length / 2);
-            Assert.Equal(12, result);
         }
     }
 }

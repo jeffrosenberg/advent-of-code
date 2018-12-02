cd52a981c3ed19c69a6fb0ff34aa16cf08f419d2
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2018-12-02 15:28:55 -0600

    2018-02: Implement a method for counting characters per line

--- javascript/src/2018/201802/checksum.js
+++ javascript/src/2018/201802/checksum.js
@@ -1,5 +1,31 @@
+// Use bitmasks for storing the number of twos and threes in a single result
+const HAS_EXACTLY_TWO = 1;
+const HAS_EXACTLY_THREE = 1 << 1;
+
 const getChecksumForLine = function(input) {
-  var result = 0;
+  const len = input.length;
+  let characters = {};
+  let result = 0;
+
+  // Count the number of occurences of each character in the line
+  for(let i = 0; i < len; i++) {
+    char = input.charAt(i);
+    if (characters.hasOwnProperty(char)) {
+      characters[char] += 1;
+    } else {
+      characters[char] = 1;
+    }
+  }
+
+  // Iterate over the counts and determine whether any characters
+  // have exactly two and/or three occurences
+  for(let char in characters) {
+    if (characters[char] == 2) {
+      result = result | HAS_EXACTLY_TWO;
+    } else if (characters[char] == 3) {
+      result = result | HAS_EXACTLY_THREE;
+    }
+  }
 
   return result;
 }

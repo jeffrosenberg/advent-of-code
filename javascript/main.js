a575e4ab51e9cca9d7cd0642e13eb88c2bb81c04
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2018-12-02 20:32:05 -0600

    2018-02: Add code to calculate the second day two output

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,8 @@ const chalk = require('chalk');
 const puzzleInputPath = '../puzzle-input';
 
 const _201701 = require('./src/2017/201701/captcha');
-const _201802 = require('./src/2018/201802/checksum');
+const _201802_checksum = require('./src/2018/201802/checksum');
+const _201802_search = require('./src/2018/201802/search');
 
 // Read the 2017 Day 01 puzzle input and output the answer
 let data = fs.readFileSync(`${puzzleInputPath}/2017/201701/puzzleInput.txt`, 'utf8');
@@ -18,7 +19,11 @@ process.stdout.write(chalk.blue.bold(result) + '\n');
 
 // Read the 2018 Day 02 puzzle input and output the answer
 data = fs.readFileSync(`${puzzleInputPath}/2018/201802/puzzleInput201802.txt`, 'utf8');
-result = _201802.getChecksum(data.split('\n'));
+inputs = data.split('\n');
 
 process.stdout.write('2018 Day 02:\n');
+result = _201802_checksum.getChecksum(inputs);
 process.stdout.write(chalk.green.bold(result) + '\n');
+
+result = _201802_search.search(inputs);
+process.stdout.write(chalk.blue.bold(result) + '\n');

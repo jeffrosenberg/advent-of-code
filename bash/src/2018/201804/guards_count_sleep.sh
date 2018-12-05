b636afe68d467987ca97d65fb653ab678737d66d
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2018-12-04 20:31:32 -0600

    2018-04: Implement script for producing minute-by-minute outputs

--- bash/src/2018/201804/guards_count_sleep.sh
+++ bash/src/2018/201804/guards_count_sleep.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-echo "10 1518-11-01 00:05"
-echo "10 1518-11-01 00:06"
-echo "10 1518-11-01 00:07"
-echo "10 1518-11-01 00:08"
-echo "10 1518-11-01 00:09"
+declare -i startInt
+declare -i endInt
+declare -i counter
+
+# Iterate over each line, splitting into variables at whitespace
+while read id date start end; do
+  # Convert the last two characters of each time into integers
+  startInt=${start:3:2}
+  endInt=${end:3:2}
+  let endInt-- # Per instructions, the ending minute should be left off
+
+  # Use `seq` to write one output line per minute between start and end
+  seq -f "$id $date 00:%02g" $startInt $endInt
+done < "${1:-/dev/stdin}"

99328e6a78d2c30926c3b3625927224b8471b4d4
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2018-12-05 16:47:37 -0600

    2018-05: Add code to output the 2018-05 result for part one

--- javascript/main.js
+++ javascript/main.js
@@ -6,6 +6,7 @@ const puzzleInputPath = '../puzzle-input';
 const _201701 = require('./src/2017/201701/captcha');
 const _201802_checksum = require('./src/2018/201802/checksum');
 const _201802_search = require('./src/2018/201802/search');
+const _201805_polymer = require('./src/2018/201805/polymer');
 
 // Read the 2017 Day 01 puzzle input and output the answer
 let data = fs.readFileSync(`${puzzleInputPath}/2017/201701/puzzleInput.txt`, 'utf8');
@@ -27,3 +28,10 @@ process.stdout.write(chalk.green.bold(result) + '\n');
 
 result = _201802_search.search(inputs);
 process.stdout.write(chalk.blue.bold(result) + '\n');
+
+// Read the 2018 Day 05 puzzle input and output the answer
+data = fs.readFileSync(`${puzzleInputPath}/2018/201805/puzzleInput201805.txt`, 'utf8');
+
+process.stdout.write('2018 Day 05:\n');
+result = _201805_polymer.react(data);
+process.stdout.write(chalk.green.bold(result.length) + '\n');

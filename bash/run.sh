99059cc6fd17a328e0fa8d11d57714868ccdaeac
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2018-12-04 21:45:24 -0600

    2018-04: Add puzzle input and code for generating part one results

--- bash/run.sh
+++ bash/run.sh
@@ -15,3 +15,10 @@ echo "2017 Day 01:"
 input="$(cat < $INPUT/2017/201701/puzzleInput.txt)"
 "$BASH_SRC_DIR/2017/201701/captcha.sh" "$input"
 "$BASH_SRC_DIR/2017/201701/captcha.sh" "$input" ${#input}/2
+
+echo
+echo "2018 Day 04:"
+"$BASH_SRC_DIR/2018/201804/guards_format_inputs.sh" "$INPUT/2018/201804/puzzleInput201804.txt" | 
+  "$BASH_SRC_DIR/2018/201804/guards_parse_inputs.sh" |
+  "$BASH_SRC_DIR/2018/201804/guards_count_sleep.sh" |
+  "$BASH_SRC_DIR/2018/201804/guards_final_results.sh"

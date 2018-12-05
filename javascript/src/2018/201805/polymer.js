15b1b1c23db52464a8b7a57f5aa0e23948f0fec2
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2018-12-05 09:43:10 -0600

    2018-05: Implement initial method for a single polymer reaction

--- javascript/src/2018/201805/polymer.js
+++ javascript/src/2018/201805/polymer.js
@@ -1,5 +1,35 @@
+const isDestroyed= (pair) => {
+  try {
+    if (pair.char != pair.prevChar && pair.char.toUpperCase() == pair.prevChar.toUpperCase()) {
+      return true;
+    } else if (pair.char != pair.nextChar && pair.char.toUpperCase() == pair.nextChar.toUpperCase()) {
+      return true;
+    }
+  }
+  catch (ex) {
+    return false;
+  }
+
+  return false;
+}
+
 const react = (input) => {
-  return 'c';
+  let chars = input.split('');
+
+  let pairs = chars.map((char, index, chars) => {
+    return {
+      index,
+      char,
+      prevChar: chars[index-1],
+      nextChar: chars[index+1],
+    };
+  });
+
+  let final_result = pairs.reduce((result, pair, index, pairs) => {
+    return result += isDestroyed(pair) ? '' : pair.char;
+  }, '');
+  
+  return final_result;
 }
 
 module.exports.react = react;
--- javascript/test/2018/20180805/polymerTest.js
+++ javascript/test/2018/20180805/polymerTest.js
@@ -3,8 +3,13 @@ const expect = chai.expect;
 const src = require('../../../src/2018/201805/polymer');
 
 describe('2018 day 05', function() {
-  it('should return "c" from an input of "cAa', function() {
+  it('should return "c" from an input of "cAa"', function() {
     var result = src.react('cAa');
     expect(result).to.equal('c');
+  });
+
+  it('should return "d" from an input of "dAa"', function() {
+    var result = src.react('dAa');
+    expect(result).to.equal('d');
   });
 });

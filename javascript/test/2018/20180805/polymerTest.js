5ac50f38e4d15771c78631f1209c1466ec5c134f
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2018-12-05 12:42:43 -0600

    2018-05: Add additional tests from the problem set, some of them currently failing

--- javascript/test/2018/20180805/polymerTest.js
+++ javascript/test/2018/20180805/polymerTest.js
@@ -3,6 +3,11 @@ const expect = chai.expect;
 const src = require('../../../src/2018/201805/polymer');
 
 describe('2018 day 05', function() {
+  it('should return an empty string from an input of "aA"', function() {
+    var result = src.react('aA');
+    expect(result).to.equal('');
+  });
+
   it('should return "c" from an input of "cAa"', function() {
     var result = src.react('cAa');
     expect(result).to.equal('c');
@@ -11,5 +16,20 @@ describe('2018 day 05', function() {
   it('should return "d" from an input of "dAa"', function() {
     var result = src.react('dAa');
     expect(result).to.equal('d');
+  });
+
+  it('should return an empty string from an input of "abBA"', function() {
+    var result = src.react('abBA');
+    expect(result).to.equal('');
+  });
+
+  it('should return "abAB" from an input of "abAB"', function() {
+    var result = src.react('abAB');
+    expect(result).to.equal('abAB');
+  });
+
+  it('should return "aabAAB" from an input of "aabAAB"', function() {
+    var result = src.react('aabAAB');
+    expect(result).to.equal('aabAAB');
   });
 });

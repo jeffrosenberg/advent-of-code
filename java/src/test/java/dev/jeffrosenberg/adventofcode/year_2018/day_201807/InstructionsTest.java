f4d4d249d2f8282e61af494e5dfe4cdbce01a5cb
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2018-12-07 14:50:11 -0600

    2018-07: Add additional tests for parsing instructions

--- java/src/test/java/dev/jeffrosenberg/adventofcode/year_2018/day_201807/InstructionsTest.java
+++ java/src/test/java/dev/jeffrosenberg/adventofcode/year_2018/day_201807/InstructionsTest.java
@@ -27,4 +27,50 @@ public class InstructionsTest {
     assertEquals(expected.vertexSet().toString(), result.vertexSet().toString());
     assertEquals(expected.edgeSet().toString(), result.edgeSet().toString());
   }
+
+  @Test
+  public void InstructionsStep3ProducesGraphWithVerticesAAndB() {
+    Instructions instructions = new Instructions();
+    instructions.parseInstruction("Step A must be finished before step B can begin.");
+    DirectedAcyclicGraph result = instructions.getSteps();
+
+    DirectedAcyclicGraph expected = new DirectedAcyclicGraph<String, DefaultEdge>(DefaultEdge.class);
+    expected.addVertex("A");
+    expected.addVertex("B");
+    expected.addEdge("A", "B");
+
+    assertEquals(expected.vertexSet().toString(), result.vertexSet().toString());
+    assertEquals(expected.edgeSet().toString(), result.edgeSet().toString());
+  }
+
+  @Test
+  public void ExampleInstructionsProduceCorrectGraph() {
+    Instructions instructions = new Instructions();
+    instructions.parseInstruction("Step C must be finished before step A can begin.");
+    instructions.parseInstruction("Step C must be finished before step F can begin.");
+    instructions.parseInstruction("Step A must be finished before step B can begin.");
+    instructions.parseInstruction("Step A must be finished before step D can begin.");
+    instructions.parseInstruction("Step B must be finished before step E can begin.");
+    instructions.parseInstruction("Step D must be finished before step E can begin.");
+    instructions.parseInstruction("Step F must be finished before step E can begin.");
+    DirectedAcyclicGraph result = instructions.getSteps();
+
+    DirectedAcyclicGraph expected = new DirectedAcyclicGraph<String, DefaultEdge>(DefaultEdge.class);
+    expected.addVertex("C");
+    expected.addVertex("A");
+    expected.addVertex("F");
+    expected.addVertex("B");
+    expected.addVertex("D");
+    expected.addVertex("E");
+    expected.addEdge("C", "A");
+    expected.addEdge("C", "F");
+    expected.addEdge("A", "B");
+    expected.addEdge("A", "D");
+    expected.addEdge("B", "E");
+    expected.addEdge("D", "E");
+    expected.addEdge("F", "E");
+
+    assertEquals(expected.vertexSet().toString(), result.vertexSet().toString());
+    assertEquals(expected.edgeSet().toString(), result.edgeSet().toString());
+  }
 }

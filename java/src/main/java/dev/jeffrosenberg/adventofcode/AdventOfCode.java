dd97875ce26616ffac0a91e53981bd502b931ac0
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2018-12-07 16:23:06 -0600

    2018-07: Add code for generating part one outputs

--- java/src/main/java/dev/jeffrosenberg/adventofcode/AdventOfCode.java
+++ java/src/main/java/dev/jeffrosenberg/adventofcode/AdventOfCode.java
@@ -4,15 +4,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+
 import dev.jeffrosenberg.adventofcode.year_2017.day_201701.Captcha;
+import dev.jeffrosenberg.adventofcode.year_2018.day_201807.Instructions;
 
 public class AdventOfCode {
   private static final String puzzleInputPath = "../puzzle-input";
 
   public static void main(String[] args) throws IOException {
+    byte[] encoded;
+
     // 2017-01
-    Path puzzleInput = Paths.get(puzzleInputPath, "2017/201701/puzzleInput.txt");
-    byte[] encoded = Files.readAllBytes(puzzleInput);
+    Path puzzleInput201701 = Paths.get(puzzleInputPath, "2017/201701/puzzleInput.txt");
+    encoded = Files.readAllBytes(puzzleInput201701);
     String input = new String(encoded, java.nio.charset.StandardCharsets.UTF_8);
 
     int captchaResult = Captcha.getCaptcha(input);
@@ -21,5 +26,18 @@ public class AdventOfCode {
 
     captchaResult = Captcha.getCaptcha(input, (input.length())/2);
     System.out.println(captchaResult);
+
+    // 2018-07
+    Path puzzleInput201807 = Paths.get(puzzleInputPath, "2018/201807/puzzleInput201807.txt");
+    List<String> input201807 = Files.readAllLines(puzzleInput201807);
+
+    Instructions instructions = new Instructions();
+    for(String lineInput : input201807) {
+      instructions.parseInstruction(lineInput);
+    }
+
+    String instructionResult = instructions.getOutput();
+    System.out.println("2018 Day 07:");
+    System.out.println(instructionResult);
   }
 }

039ea0c08950e2d61021ef87fd1cd630fe2eda2a
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2018-12-08 21:42:05 -0600

    2018-07: Implement methods to parse input and generate metadata sums

--- csharp/AdventOfCode.Tests/2018/201808/NavigationTest.cs
+++ csharp/AdventOfCode.Tests/2018/201808/NavigationTest.cs
@@ -11,9 +11,43 @@ namespace AdventOfCode.Tests
         [InlineData(new int[] { 2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2, }, 138)]
         public void CorrectlyProvidesMetadataSumForPart1(int[] input, int expected)
         {
-            Navigation navigation = new Navigation(input);
-            int result = navigation.GetMetadataSum();
+            NavigationNode rootNode = new NavigationNode(input);
+            int result = rootNode.GetMetadataSum();
             Assert.Equal(expected, result);
         }
+
+        // This seemed to be working, but I didn't take the time to
+        // default a proper Equals() method for NavigationNode
+        //[Fact]
+        //public void ProducesFullTreeRecursively()
+        //{
+        //    // Expected
+        //    NavigationNode D = new NavigationNode
+        //    {
+        //        Metadata = new List<int> { 99 }
+        //    };
+
+        //    NavigationNode C = new NavigationNode
+        //    {
+        //        ChildNodes = new List<NavigationNode> { D },
+        //        Metadata = new List<int> { 2 }
+        //    };
+
+        //    NavigationNode B = new NavigationNode
+        //    {
+        //        Metadata = new List<int> { 10, 11, 12 }
+        //    };
+
+        //    NavigationNode A = new NavigationNode
+        //    {
+        //        ChildNodes = new List<NavigationNode> { B, C },
+        //        Metadata = new List<int> { 1, 1, 2 }
+        //    };
+
+        //    // Actual
+        //    int[] input = new int[] { 2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2 };
+        //    NavigationNode rootNode = new NavigationNode(input);
+        //    Assert.Equal(A, rootNode);
+        //}
     }
 }
--- csharp/AdventOfCode/2018/201808/Navigation.cs
+++ csharp/AdventOfCode/2018/201808/Navigation.cs
@@ -4,18 +4,52 @@ using System.Linq;
 
 namespace AdventOfCode
 {
-    public class Navigation
+    public class NavigationNode
     {
-        private readonly IEnumerable<int> Input;
+        private Queue<int> _input;
+        public List<NavigationNode> ChildNodes { get; set; }
+        public List<int> Metadata { get; set; }
 
-        public Navigation(IEnumerable<int> input)
+        public NavigationNode() : this(new int[0], null) { }
+        public NavigationNode(IEnumerable<int> input) : this(input, null) { }
+        public NavigationNode(IEnumerable<int> input, NavigationNode parentNode)
         {
-            this.Input = input;
+            ChildNodes = new List<NavigationNode>();
+            Metadata = new List<int>();
+
+            if (input.Any())
+            {
+                _input = new Queue<int>(input);
+                if (parentNode == null)
+                {
+                    ParseInput();
+                }
+            }
+        }
+
+        protected Queue<int> ParseInput()
+        {
+            int childNodes = _input.Dequeue();
+            int metadataNodes = _input.Dequeue();
+
+            for (int i = 0; i < childNodes; i++)
+            {
+                NavigationNode newNode = new NavigationNode(_input, this);
+                _input = newNode.ParseInput();
+                ChildNodes.Add(newNode);
+            }
+
+            for (int i = 0; i < metadataNodes; i++)
+            {
+                Metadata.Add(_input.Dequeue());
+            }
+
+            return _input;
         }
 
         public int GetMetadataSum()
         {
-            throw new NotImplementedException();
+            return Metadata.Sum()+ ChildNodes.Sum(n => n.GetMetadataSum());
         }
     }
 }

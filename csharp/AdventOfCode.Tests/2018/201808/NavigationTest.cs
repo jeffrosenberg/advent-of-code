fafb79dde5eb713de0917f20dcdcddc7dfbdb09c
Jeff Rosenberg <rosenberg.jeff@gmail.com>
2018-12-08 20:36:59 -0600

    2018-08: Add initial failing test

---
+++ csharp/AdventOfCode.Tests/2018/201808/NavigationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using AdventOfCode;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Tests
+{
+    public class NavigationTest
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2, }, 138)]
+        public void CorrectlyProvidesMetadataSumForPart1(int[] input, int expected)
+        {
+            Navigation navigation = new Navigation(input);
+            int result = navigation.GetMetadataSum();
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ csharp/AdventOfCode/2018/201808/Navigation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Navigation
+    {
+        private readonly IEnumerable<int> Input;
+
+        public Navigation(IEnumerable<int> input)
+        {
+            this.Input = input;
+        }
+
+        public int GetMetadataSum()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

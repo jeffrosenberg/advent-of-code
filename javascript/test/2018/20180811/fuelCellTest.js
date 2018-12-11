beddcb44f05360d20e68aef3bbec55e5d744b375
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2018-12-11 11:08:58 -0600

    2018-11: Add additional failing tests from challenge examples

--- javascript/test/2018/20180811/fuelCellTest.js
+++ javascript/test/2018/20180811/fuelCellTest.js
@@ -7,4 +7,19 @@ describe('2018 day 11', function() {
     let result = src.powerByCoordinate(3, 5, 8);
     expect(result).to.equal(4);
   });
+
+  it('should return -5 from fuel cell (122,79) with serial number 57', function() {
+    let result = src.powerByCoordinate(122, 79, 57);
+    expect(result).to.equal(-5);
+  });
+
+  it('should return 0 from fuel cell (217,196) with serial number 39', function() {
+    let result = src.powerByCoordinate(217, 196, 39);
+    expect(result).to.equal(0);
+  });
+
+  it('should return 4 from fuel cell (101,153) with serial number 71', function() {
+    let result = src.powerByCoordinate(101, 153, 71);
+    expect(result).to.equal(4);
+  });
 });

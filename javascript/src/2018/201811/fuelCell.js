7a33f242a2693d476ba73fe846b36fcaeb4fad80
Jeff Rosenberg <jeff.rosenberg@icfolson.com>
2018-12-11 21:25:24 -0600

    2018-11: Add comments

--- javascript/src/2018/201811/fuelCell.js
+++ javascript/src/2018/201811/fuelCell.js
@@ -27,23 +27,28 @@ const powerByCoordinate = (x, y, serial) => {
 
 const maxPowerBySerialNumber = (serial, gridHorizSize, gridVertSize, resultSize=3) => {
   let grid = [];
-  let result = { x: 0, y: 0, power: 0, totalPower: 0, resultSize, };
+  let result = { x: 0, y: 0, power: 0, totalPower: 0, resultSize, }; // Always store the current max
   let size = resultSize - 1; // For getting grid size, because the current row counts as 1
 
+  // Loop through x and y coordinates, populating an array of results as we go
   for (x = gridHorizSize; x > 0 ; x--) {
     for (y = gridVertSize; y > 0 ; y--) {
-      let power = powerByCoordinate(x, y, serial);
-      grid.push({ x, y, power });
+      let power = powerByCoordinate(x, y, serial); // For each coordinate, calculate power
+      grid.push({ x, y, power }); // Store the power for each coordinate in the grid array
+
+      // When a large enough space is populated, calculate total power
       if (x <= (gridHorizSize-size) && y <= (gridVertSize-size)) {
         let totalPower = 
           grid.reduce((total, item) => {
             return total += ((item.y <= y+size && item.y >= y) ? item.power : 0)
           }, 0);
         if (totalPower > result.totalPower) {
-          result = { x, y, power, totalPower, resultSize };
+          result = { x, y, power, totalPower, resultSize }; // Replace the result when appropriate
         }
       }
     }
+
+    // To keep processing time down, remove each x coordinate once it's no longer needed
     if (x <= (gridHorizSize-size)) {
       grid.splice(0, gridVertSize);
     }
